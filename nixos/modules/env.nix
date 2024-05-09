9bd7a0c05453267b7d6e8dff4a7ca1ac884e3e2f
asserthq <sanya2003ogorodov@gmail.com>
2024-05-09 21:03:07 +0700

    a

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -17,5 +17,10 @@
     XDG_SESSION_DESKTOP = "Hyprland";
     GTK_USE_PORTAL = "1";
     NIXOS_XDG_OPEN_USE_PORTAL = "1";
+    
+    XDG_CACHE_HOME = "\${HOME}/.cache";
+	  XDG_CONFIG_HOME = "\${HOME}/.config";
+	  #XDG_BIN_HOME = "\${HOME}/.local/bin";
+	  XDG_DATA_HOME = "\${HOME}/.local/share";
   };
 }
