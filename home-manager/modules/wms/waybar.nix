6d0f1e381b92e73e24aa53338dfc49cbbda419f0
asserthq <sanya2003ogorodov@gmail.com>
2024-05-09 21:09:43 +0700

    a

--- home-manager/modules/wms/waybar.nix
+++ home-manager/modules/wms/waybar.nix
@@ -5,5 +5,20 @@
     package = pkgs.waybar.overrideAttrs (oldAttrs: {
       mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
     });
+    settings = {
+      mainBar = {
+        height = 20;
+        layer = "top";
+        modules-left = [ "clock" ];
+        modules-center = [ "clock" ];
+        modules-right = [ "clock" ];
+
+        "clock" = {
+          format = "{:%H:%M}";
+          format-alt = "{:%b %d %Y}";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
+      };
+    };
   };
 }
