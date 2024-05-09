0a807c04a055ec582ebfff07d8e61c678e7152f5
asserthq <sanya2003ogorodov@gmail.com>
2024-05-09 19:08:08 +0700

    c

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,10 +2,14 @@
   imports = [
     ./modules/bundle.nix
   ];
-
+  
   home = {
     username = "sanya";
     homeDirectory = "/home/sanya";
     stateVersion = "23.11";
+  };
+
+  home-manager.extraSpecialArgs = {
+    inherit unstable
   };
 }
